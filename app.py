2cbf6eca2800c0b1d6e07f846cf660f670082c0b
aji <daemon.django@gmail.com>
2020-10-03 11:17:33 +0000

    edited index.html but not app.py

---
+++ app.py
@@ -0,0 +1,27 @@
+from flask import Flask, render_template, url_for
+import os
+import json
+import textwrap
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+	return render_template('index.html')
+
+#note that it is easier here in os.walk to define the directory, so we'll add an argument for 'user' here.
+def get_static_file_map():
+	file_map= {}
+	for dirname, subdirs, files in os.walk('static/honte'):
+		file_map[dirname] = []
+		for name in files:
+			file_map[dirname].append(os.path.join(dirname[7::], name))
+	return file_map
+
+@app.route('/inktober')
+def userGallery():
+	file_map = get_static_file_map()
+	return render_template('gallery.html', file_map=file_map,)
+
+if __name__ == '__main__':
+	app.run(debug=false)
